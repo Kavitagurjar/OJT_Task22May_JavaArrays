424c26107d14389c6d1f5520e40b0f54fe4fc465
Kavita Gurjar <121787703+Kavitagurjar@users.noreply.github.com>
2024-06-03 10:20:10 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,41 @@
+
+// public class Arrays {
+//     public static void main(String[] args) {
+//         String[] States = { "U.P.", "U.K.", "Bihar", "Himanchal Pradesh" };
+//         System.out.println(States[0]);
+//         System.out.println(States[1]);
+//         System.out.println(States[2]);
+//         System.out.println(States[3]);
+
+//         String[] phone = { "vivo", "samsung", "oppo" };
+//         System.out.println(phone.length);
+
+//         for (int i = 0; i < phone.length; i++)
+//             System.out.println(phone[i]);
+
+//     }
+// }
+
+
+
+// public class Arrays{
+//     public static void main(String args[]){
+//         int marks [] = {78, 56, 91, 45, 22, 39, 88,99};
+//         float avg, sum = 0;
+//         int length = marks.length;
+//         for(int result : marks){
+//             sum+= result;
+//         }
+//         avg = sum/length;
+//         System.out.println("The average is : " + avg);
+//     }
+// }
+
+public class Arrays{
+    public static void main(String args[]){
+        String [] name = {"Kavita", "Seema", "Manisha", "Kunjesh", "Ramakant"};
+       for(String i : name){
+            System.out.println(i);
+    }
+    }
+} 
